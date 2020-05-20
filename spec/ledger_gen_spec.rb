96813292893a4c691bcb625550313e04cd3a0985
Pete Keen <pete@petekeen.net>
2020-05-19 21:05:52 -0400

    Add a spec for typecheck

--- spec/ledger_gen_spec.rb
+++ spec/ledger_gen_spec.rb
@@ -1,8 +1,16 @@
 # typed: false
 require "spec_helper"
+require "open3"
 
 describe LedgerGen do
   it "has a version number" do
     expect(LedgerGen::VERSION).not_to be nil
+  end
+
+  it "should typecheck" do
+    stdout, stderr, status = Open3.capture3('bundle exec srb tc')
+    if status.exitstatus != 0
+      raise stdout + stderr
+    end
   end
 end
